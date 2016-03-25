2b2debe75ac21919c5f2897265882fc504fc0be7
klouse1988 <klouse1988@gail.com>
2016-03-26 07:38:00 +0800

    11

--- weekdays.py
+++ weekdays.py
@@ -2,9 +2,32 @@ import numpy as np
 from datetime import datetime
 
 def strdate2num(s):
-    return datetime.strptime(s, '\"%Y-%m-%d').date().weekday()
+    return datetime.strptime(s, '%Y-%m-%d').date().weekday()
 
-dates, close = np.loadtxt("data.csv", delimiter=',', usecols=(0,5), converters={0:strdate2num}, unpack=True)
+def str2num(s):
+    if s == '--':
+        return 0
+    else:
+        return float(s)
 
 
-print dates, close
+def dayAverage():
+    dates, close, w = np.loadtxt("data.csv", delimiter=',', usecols=(0,5,11), converters={0:strdate2num, 11:strExpect}, unpack=True)
+
+    dayAver = []
+    for i in range(5):
+        index = np.where(dates == i)
+        daysData = np.take(close, index)
+        dayAver.append(np.mean(daysData))
+        print "%d: %s" % (i, daysData)
+
+    return dayAver
+
+
+
+
+
+
+
+
+
